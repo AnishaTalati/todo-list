858add15df8248586ffb028c1f9cc364e5e925df
Nish <talatianisha@gmail.com>
2021-11-09 18:39:14 +0000

    added components and json for todo contents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Header from './Header'
+import ToDoList from './ToDoList';
+import data from "./data.json"
 
 function App() {
+  const [toDoList, setToDoList] = useState(data)
+
   return (
     <div className="App">
-    
+    <Header/>
+    <ToDoList toDoList={toDoList}/>
     </div>
   );
 }
